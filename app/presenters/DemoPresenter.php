8ca23d64e1a965119be38ae917138be9e2559b09
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-01-30 13:02:06 +0100

    DemoPresenter: additional examples

--- app/presenters/DemoPresenter.php
+++ app/presenters/DemoPresenter.php
@@ -10,14 +10,45 @@ use Nette\Diagnostics\Debugger;
 final class DemoPresenter extends UI\Presenter
 {
 
+	public function renderDefault()
+	{
+		$this->dump($this->getComponent('root'), 'Získání komponenty s názvem root');
+		$this->dump($this['root']); // ekvivalentní zápis
+
+		$this->dump($root = $this['root'], 'Root');
+
+		$this->dump($this['root']['a'], 'Získání komponenty "a" uvnitř "root"');
+		$this->dump($this['root-a']);
+		$this->dump($root['a']);
+
+		$this->dump($this['root']['c']['y'], 'Y');
+		$this->dump($root['c']['y']);
+		$this->dump($root['c-y']);
+
+		$this->dump($root->components, 'Komponenty obsažené v komponentě root');
+
+		$this->dump($this['root-c-x']->parent, 'Rodič X');
+
+		$this->dump($this['root-c-x']->parent->parent, 'Rodič rodiče X');
+
+		$this->dump($this['root-c-x']->parent->parent->parent, 'Rodič rodiče rodiče X');
+
+		// $this['bar']; // Component with name 'bar' does not exist.
+
+		$this->dump($this['root-a-foo-foo'], 'Hraní s createComponentFoo()');
+	}
+
 	protected function createComponentRoot()
 	{
 		$control = new BoxControl();
 		$control->addComponent(new BoxControl, 'a');
+		// $root->addComponent(new BoxControl, 'a'); // Component with name 'a' already exists.
 		$control->addComponent(new BoxControl, 'b');
 		$control->addComponent(new BoxControl, 'c');
 		$control['c']->addComponent(new BoxControl, 'x');
 		$control['c']->addComponent(new BoxControl, 'y');
+
+		// $control->removeComponent($control['b']); // odstraní komponentu b
 
 		return $control;
 	}
