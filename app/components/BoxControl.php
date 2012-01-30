a41c167d3ac057a58f4c95249e91ce79b55b4ce7
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-01-30 12:57:45 +0100

    added BoxControl

---
+++ app/components/BoxControl.php
@@ -0,0 +1,35 @@
+<?php
+
+use Nette\Application\UI;
+
+
+/**
+ * Komponenta pro demonstraci hierarchické struktury.
+ *
+ * @author   Jan Tvrdík
+ */
+class BoxControl extends UI\Control
+{
+
+	/**
+	 * Metody render() je automaticky volaná při vykreslování pomocí Latte makra {control}
+	 */
+	public function render()
+	{
+		// Stejně jako v presenteru je i v komponentě k dispozici šablona pomocí $this->template.
+		// Navíc je ale potřeba ručně nastavit cestku k šabloně a zavolat metodu render()
+		$this->template->setFile(__DIR__ . '/BoxControl.latte');
+		$this->template->render();
+	}
+
+	/**
+	 * Továrnička na "foo". Bude automaticky zavolaná při pokusu o získání komponenty "foo".
+	 *
+	 * @return   BoxControl
+	 */
+	protected function createComponentFoo()
+	{
+		return new BoxControl();
+	}
+
+}
