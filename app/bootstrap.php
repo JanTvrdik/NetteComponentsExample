4ea3aa91879df512e8be02cd99ca15f79787a88c
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-01-30 13:11:00 +0100

    added loading of config-local.neon + example

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,6 +10,7 @@ $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->createRobotLoader()->addDirectory(array(APP_DIR, LIBS_DIR))->register();
 $configurator->addConfig(__DIR__ . '/config.neon');
+$configurator->addConfig(__DIR__ . '/config-local.neon');
 
 $container = $configurator->createContainer();
 $container->router[] = new Route('index.php', 'Demo:default', Route::ONE_WAY);
