edef487cca6dbc08c2bc6690ab2bf262c5223ed1
Jan Tvrdik <jan.tvrdik@gmail.com>
2015-01-10 12:00:02 +0100

    moved config files to app/config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,7 +7,7 @@ $configurator = new Nette\Configurator;
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->createRobotLoader()->addDirectory(__DIR__)->register();
-$configurator->addConfig(__DIR__ . '/config.neon');
-$configurator->addConfig(__DIR__ . '/config-local.neon');
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(__DIR__ . '/config/config-local.neon');
 
 return $configurator->createContainer();
