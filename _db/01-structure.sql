392796bb284426ea1990c89c4d75841f709ca4ac
Jan Tvrdik <jan.tvrdik@gmail.com>
2015-01-10 12:53:53 +0100

    db: removed unnecessary auto_increment declaration

--- _db/01-structure.sql
+++ _db/01-structure.sql
@@ -11,7 +11,7 @@ CREATE TABLE `articles` (
   `title` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 DROP TABLE IF EXISTS `comments`;
